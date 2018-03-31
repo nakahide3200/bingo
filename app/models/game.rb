ba2bcfa860be9086d2f8cd50a325cca38948355c
hidetoshinakayama1 <nakahide3200@gmail.com>
2018-04-01 03:20:53 +0900

    gameのバリデーション。

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
 class Game < ApplicationRecord
   has_many :entries, dependent: :destroy
+
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :start_time, presence: true
+
+  validate :start_time_should_be_after_now
+
+  private
+
+  def start_time_should_be_after_now
+    if start_time < DateTime.now
+      errors.add(:start_time, 'は現在時刻以降を指定してください。')
+    end
+  end
 end
