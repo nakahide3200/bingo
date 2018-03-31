1cdef37d3ff6b0dcd6e83a4da9b4824abcd54aee
hidetoshinakayama1 <nakahide3200@gmail.com>
2018-03-31 23:52:51 +0900

    ゲーム一覧画面、ゲーム画面追加。

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,16 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: %i[show]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+end
