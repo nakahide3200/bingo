72622ea0222cb223ab9135a03a4606e1658e548c
hidetoshinakayama1 <nakahide3200@gmail.com>
2018-04-06 00:49:04 +0900

    チャンネルをゲーム毎に分ける。

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,13 +1,13 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'notifications_channel'
+    stream_from "notifications_channel_#{params['game_id']}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def notify_lot
-  	ActionCable.server.broadcast 'notifications_channel', nil
+  def notify_lot(data)
+    ActionCable.server.broadcast "notifications_channel_#{data['game_id']}", nil
   end
 end
